6a1efbb0ee813ddf266eac20fc2e91a66b033f5d
unoitdev <unoitdev@gmail.com>
2023-08-17 17:54:59 +0800

    add dropColumn deleted

--- database/migrations/2023_08_17_074637_add_deleted_column_to_sales_details_table.php
+++ database/migrations/2023_08_17_074637_add_deleted_column_to_sales_details_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('sales_details', function (Blueprint $table) {
-            //
+            $table->dropColumn('deleted');
         });
     }
 };

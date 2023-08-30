3fb4bdc77fd971281a7e90e3d446b2157d785caf
unoitdev <unoitdev@gmail.com>
2023-08-30 09:05:30 +0800

    remove search icon from navbar

--- config/adminlte.php
+++ config/adminlte.php
@@ -294,7 +294,7 @@ return [
         [
             'type'         => 'navbar-search',
             'text'         => 'search',
-            'topnav_right' => true,
+            'topnav_right' => false,
         ],
         [
             'type'         => 'fullscreen-widget',

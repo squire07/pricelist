6353df408343e3d91aca90c372dc107f83140552
unoitdev <unoitdev@gmail.com>
2023-09-12 13:56:02 +0800

    Add history component; include history at blade

--- resources/views/SalesOrder/show.blade.php
+++ resources/views/SalesOrder/show.blade.php
@@ -75,31 +75,7 @@
         </div>
     </div>
 
-    <div class="card">
-        <div class="card-header text-bold">History</div>
-        <div class="card-body">
-            <table class="table table-bordered table-hover table-striped" width="100%">
-                <thead>
-                    <tr>
-                        <th class="text-center">Event</th>
-                        <th class="text-center">Remarks</th>
-                        <th class="text-center">Created By</th>
-                        <th class="text-center">Created At</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($histories as $history)
-                        <tr>
-                            <td>{{ $history->event_name }}</td>
-                            <td>{{ $history->remarks }}</td>
-                            <td class="text-center">{{ $history->created_by }}</td>
-                            <td class="text-center">{{ $history->created_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+    @include('components.history')
 
     {{-- hidden form to submit SO for invoicing --}}
     <form id="form_for_invoicing" method="POST">

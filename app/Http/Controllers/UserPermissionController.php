2a7a38ff79a6cdd2f652d65aeeca1173ec4d22b4
unoitdev <unoitdev@gmail.com>
2023-10-10 11:39:37 +0800

    UserPermissionController - remove comments

--- app/Http/Controllers/UserPermissionController.php
+++ app/Http/Controllers/UserPermissionController.php
@@ -49,39 +49,6 @@ class UserPermissionController extends Controller
      */
     public function edit(UserPermission $userPermission, $uuid)
     {
-        // $sdf = '48-1-1-1';
-        // $explode = explode('-', $sdf);
-        // dump($explode);
-
-        // // get user by id
-        // $permission = UserPermission::whereUserId($explode[0])->first();
-        // dump($permission);
-
-        // // get the user's permission
-        // // Decode JSON into a PHP associative array
-        // $data = json_decode($permission->user_permission, true);
-        // dump($data);
-
-        // dump($data[$explode[1]]);
-        // dump($data[$explode[1]][$explode[2]]);
-        // dump($data[$explode[1]][$explode[2]] == 0);
-
-        // if (isset($data[$explode[1]]) && isset($data[$explode[1]][$explode[2]])) {
-            
-        //     $data[$explode[1]][$explode[2]] = ($data[$explode[1]][$explode[2]] == 1) ? 0 : 1;
-
-        //     // save the json_data
-        //     $permission->user_permission = json_encode($data);
-        //     $permission->update();
-
-        //     return json_encode(['message'=>'updated']);
-        // } else {
-        //     return json_encode(['message'=>'error']);
-        // }
-
-        // dd('stop');
-
-
         $user = User::with('permission')->whereUuid($uuid)->first();
         $modules = PermissionModule::whereDeleted(false)->get();
         return view('user_permission.edit', compact('user','modules'));

ad70d7ca05e790181e3ca79d869863b11f5c170b
unoitdev <unoitdev@gmail.com>
2023-10-12 22:37:45 +0800

    Seeder - transaction type or price list module in erpnext

--- database/seeders/TransactionTypeSeeder.php
+++ database/seeders/TransactionTypeSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\TransactionType;
+use App\Helpers\Helper;
 
 class TransactionTypeSeeder extends Seeder
 {
@@ -15,15 +16,41 @@ class TransactionTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $transaction_types = [
-            'Standard Selling',
-            'Distributor Price'
-        ];
+        // $transaction_types = [
+        //     'Standard Selling',
+        //     'Distributor Price',
+        //     'Unopreneur\'s Selling',
+        //     'UBC\'s Selling',
+        //     'UBC Price',
+        //     'UPC Price',
+        //     'Unoshop SRP Price',
+        //     'Shopee SRP Price',
+        //     'LazMall SRP Price',
+        //     'EStore SRP Price',
+        //     'EStore Distributor Price',
+        //     'Credit Card Distributor',
+        //     'Credit Card UPC Price',
+        //     'UNO Bilis Serbisyo Distributor Price',
+        //     'UNO Bilis Serbisyo UPC Price',
+        //     'Onelifestyle Distributor Price',
+        //     'Onelifestyle UPC Price',
+        //     'Buy 1 Take 1 Promo',
+        //     'Promo Free Regular Package',
+        //     'Promo Free Regular RS',
+        //     'Promo Free New UPC',
+        //     'Promo Free New RS',
+        //     'Promo Free Sign UBC Package',
+        //     'Promo Free Sign UBC RS',
+        // ];
 
-        foreach($transaction_types as $key => $name) {
+        // ERPNext 
+        $param = '/api/resource/Price List?limit=500&filters=[["selling","=","1"]]';
+        $data = Helper::get_erpnext_data($param);
+
+        foreach($data['data'] as $key => $price_lists){
             TransactionType::create([
                 'uuid' => Str::uuid(),
-                'name' => $name,
+                'name' => $price_lists['name'],
                 'created_at' => Carbon::now()->toDateTimeString(),
                 'updated_at' => Carbon::now()->toDateTimeString(),
                 'created_by' => 'System',

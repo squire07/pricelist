bff1b160e2223728b69e9ab9a784081974d369fd
unoitdev <unoitdev@gmail.com>
2023-10-25 17:32:19 +0800

    Sales Order and Sales Invoice - for invoiceing - default sort SO no column; remove sorting at first column

--- resources/views/SalesOrder/index.blade.php
+++ resources/views/SalesOrder/index.blade.php
@@ -131,6 +131,7 @@
             paging: true,
             searching: true,
             lengthMenu: [[10, 25, 50, -1], ['10 rows', '25 rows', '50 rows', "Show All"]],  
+            order: [[1, 'desc']],
             buttons: [
                 {
                     extend: 'pageLength',

7a7c732b869515cdbed67bc12dd452c96e1f39c0
unoitdev <unoitdev@gmail.com>
2023-11-02 20:07:51 +0800

    Left Side Nav - update icons

--- resources/views/components/left-custom-sidebar.blade.php
+++ resources/views/components/left-custom-sidebar.blade.php
@@ -45,31 +45,31 @@
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="{{ url('sales-invoice/for-invoice') }}" class="nav-link {{ $url_segment_2 == 'for-invoice' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-file-invoice-dollar nav-icon"></i>
                         <p>For Invoice</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('sales-invoice/released') }}" class="nav-link {{ $url_segment_2 == 'released' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-external-link-alt nav-icon"></i>
                         <p>Released</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('sales-invoice/for-validation') }}" class="nav-link {{ $url_segment_2 == 'for-validation' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-clipboard-check nav-icon"></i>
                         <p>For Validation</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('sales-invoice/cancelled') }}" class="nav-link {{ $url_segment_2 == 'cancelled' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-window-close nav-icon"></i>
                         <p>Cancelled</p>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" style="border-bottom: 1px solid #4f5962;">
                     <a href="{{ url('sales-invoice/all') }}" class="nav-link {{ $url_segment_2 == 'all' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-list-alt nav-icon"></i>
                         <p>All</p>
                     </a>
                 </li>
@@ -81,67 +81,67 @@
         {{-- SUPPORT MODULES MENU: Sub menus are sorted alphabetically --}}
         <li class="nav-item {{ in_array($url_segment_1, $support_modules) ? 'menu-open':'' }}">
             <a href="#" class="nav-link {{ in_array($url_segment_1, $support_modules) ? 'active':'' }}" target="_self">
-                <i class="nav-icon fas fa-fw fa-share"></i>
+                <i class="nav-icon fas fa-project-diagram"></i>
                 <p>Support Modules<i class="right fas fa-angle-left"></i></p>
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="{{ url('branches') }}" class="nav-link {{ $url_segment_1 == 'branches' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-sitemap nav-icon"></i>
                         <p>Branches</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('companies') }}" class="nav-link {{ $url_segment_1 == 'companies' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-building nav-icon"></i>
                         <p>Companies</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('distributors') }}" class="nav-link {{ $url_segment_1 == 'distributors' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-user-friends nav-icon"></i>
                         <p>Distributors</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('items') }}" class="nav-link {{ $url_segment_1 == 'items' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-cubes nav-icon"></i>
                         <p>Items</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('roles') }}" class="nav-link {{ $url_segment_1 == 'roles' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-user-tag nav-icon"></i>
                         <p>Roles</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('sales-invoice-assignment') }}" class="nav-link {{ $url_segment_1 == 'sales-invoice-assignment' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-address-card nav-icon"></i>
                         <p>Sales Invoice Assignment</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('shipping-fee') }}" class="nav-link {{ $url_segment_1 == 'shipping-fee' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-shipping-fast nav-icon"></i>
                         <p>Shipping Fee</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{  url('transaction-types')  }}" class="nav-link {{ $url_segment_1 == 'transaction-types' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-receipt nav-icon"></i>
                         <p>Transaction Types</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('payment-methods') }}" class="nav-link {{ $url_segment_1 == 'payment-methods' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-credit-card nav-icon"></i>
                         <p>Payment Methods</p>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" style="border-bottom: 1px solid #4f5962;">
                     <a href="{{ url('users') }}" class="nav-link {{ in_array($url_segment_1, ['users','permissions']) ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="fas fa-user-lock nav-icon"></i>
                         <p>Users</p>
                     </a>
                 </li>
@@ -152,33 +152,31 @@
         {{-- REPORTS MENU: Sub menus are sorted alphabetically --}}
         <li class="nav-item {{ $url_segment_1 == 'reports' ? 'menu-open':'' }}">
             <a href="#" class="nav-link {{ $url_segment_1 == 'reports' ? 'active':'' }}" target="_self">
-                <i class="nav-icon fas fa-fw fa-share"></i>
-
-                <i class=" "></i>
+                <i class="nav-icon fas fa-registered"></i>
                 <p>Reports<i class="right fas fa-angle-left"></i></p>
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="{{ url('reports/build-report') }}" class="nav-link {{ $url_segment_2 == 'build-report' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-file-alt nav-icon"></i>
                         <p>Build Report</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('reports/logs') }}" class="nav-link {{ $url_segment_2 == 'logs' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-file-alt nav-icon"></i>
                         <p>Logs</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('reports/stock-card') }}" class="nav-link {{ $url_segment_2 == 'stock-card' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-file-alt nav-icon"></i>
                         <p>Stock Card</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ url('reports/transaction-listing') }}" class="nav-link {{ $url_segment_2 == 'transaction-listing' ? 'active':'' }}" target="_self">
-                        <i class="far fa-circle nav-icon"></i>
+                        <i class="far fa-file-alt nav-icon"></i>
                         <p>Transaction Listing</p>
                     </a>
                 </li>
@@ -186,7 +184,7 @@
         </li>
 
 
-        <li class="nav-header">ACCOUNT SETTINGS</li>
+        {{-- <li class="nav-header">ACCOUNT SETTINGS</li>
 
         <li class="nav-item">
             <a href="#" class="nav-link" target="_self">
@@ -199,7 +197,7 @@
                 <i class="nav-icon fas fa-fw fa-lock "></i>
                 <p>Change Password</p>
             </a>
-        </li>
+        </li> --}}
         
     </ul>
 </nav>

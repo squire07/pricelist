69e994e8d3e535979ec954d947b935e87a546535
unoitdev <unoitdev@gmail.com>
2023-11-08 23:10:57 +0800

    Payload - model and migration only

---
+++ database/migrations/2023_11_03_214349_create_payloads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payloads', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('bcid')->nullable();
+            $table->string('distributor')->nullable();
+            $table->longText('so')->nullable();
+            $table->longText('si')->nullable();
+            $table->string('nuc_points')->nullable();
+            $table->string('distributor_response')->nullable();
+            $table->string('so_response')->nullable();
+            $table->string('si_response')->nullable();
+            $table->string('nuc_points_response')->nullable();
+            $table->tinyInteger('deleted')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('created_by',55)->nullable();
+            $table->string('updated_by',55)->nullable();
+            $table->string('deleted_by',55)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payloads');
+    }
+};

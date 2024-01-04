a9289bbb3cb8c3d321ec6ceb45da8457567f9e0a
unoitdev <unoitdev@gmail.com>
2024-01-04 10:51:57 +0800

    NUC - migration - add comment on status

--- database/migrations/2023_11_24_152147_create_nucs_table.php
+++ database/migrations/2023_11_24_152147_create_nucs_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('branch')->nullable();
             $table->string('oid')->nullable(); // invoice number
             $table->string('state', 15)->nullable();
-            $table->tinyInteger('status')->default(0); // 0 - uncredited; 1 - credited; 2 - cancelled;
+            $table->tinyInteger('status')->default(0); // 0 - uncredited; 1 - credited; 2 - cancelled; 3 - on-hold (ex. cod)
             $table->tinyInteger('deleted')->default(false);
             $table->timestamps();
             $table->softDeletes();

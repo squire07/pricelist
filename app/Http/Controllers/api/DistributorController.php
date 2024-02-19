bca07afbe221544f67848c51a661a058edbda1c6
unoitdev <unoitdev@gmail.com>
2024-02-19 09:30:04 +0800

    API - add get_maintained_distributor_by_id function - used by Sales Order module for ubc and upc transactions

--- app/Http/Controllers/api/DistributorController.php
+++ app/Http/Controllers/api/DistributorController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Distributor;
+use App\Models\MaintainedMember;
+use Carbon\Carbon;
 
 class DistributorController extends Controller
 {
@@ -14,5 +16,24 @@ class DistributorController extends Controller
     public function get_distributor_by_id($bcid)
     {
         return Distributor::whereDeleted(false)->whereBcid($bcid)->get();
+    }
+
+    public function get_maintained_distributor_by_id($bcid)
+    {
+        $month = Carbon::now()->format('n');
+        $year = Carbon::now()->year;
+
+        $maintained = MaintainedMember::whereDeleted(false)
+                    ->where('bcid', $bcid)
+                    ->where('year', $year)
+                    ->where('month', $month)
+                    ->first();
+
+        // get the member details
+        if($maintained) {
+            return Distributor::whereDeleted(false)->whereBcid($bcid)->get();
+        } else {
+            return [];
+        }
     }
 }

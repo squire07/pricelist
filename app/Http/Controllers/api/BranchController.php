10454ec4fc584ce799239255abee7e6ba7f75286
unoitdev <unoitdev@gmail.com>
2024-02-19 09:25:30 +0800

    Sales Invoice Assignment - branch api update for second dropdown

--- app/Http/Controllers/api/BranchController.php
+++ app/Http/Controllers/api/BranchController.php
@@ -16,18 +16,26 @@ class BranchController extends Controller
         // auth will not work under api
         $auth_user = User::whereId($auth_user_id)->first();
 
+        $cashier_user = User::whereId($cashier_id)->first();
 
         $active_companies = Company::whereStatusId(8)->pluck('id');
         $active_branches = Branch::whereStatusId(8)->pluck('id');
 
-        $auth_user_active_company_ids = array_intersect(explode(',', $auth_user->company_id), $active_companies->toArray());
-        $auth_user_branches_by_active_company = Branch::whereIn('company_id', $auth_user_active_company_ids)->pluck('id');
+        if(in_array($auth_user->role_id, [11,12])) {
+            $final_branches = Branch::whereStatusId(8)
+                                ->whereIn('company_id', $active_companies->toArray())
+                                ->whereIn('id', explode(',',$cashier_user->branch_id))
+                                ->pluck('id');
+        } else {
+            $auth_user_active_company_ids = array_intersect(explode(',', $auth_user->company_id), $active_companies->toArray());
+            $auth_user_branches_by_active_company = Branch::whereIn('company_id', $auth_user_active_company_ids)->pluck('id');
 
-        $final_branches_sp1 = array_intersect($auth_user_branches_by_active_company->toArray(), $active_branches->toArray());
-        $final_branches_sp2 = array_intersect($auth_user_branches_by_active_company->toArray(), $final_branches_sp1);
+            $final_branches_sp1 = array_intersect($auth_user_branches_by_active_company->toArray(), $active_branches->toArray());
+            $final_branches_sp2 = array_intersect($auth_user_branches_by_active_company->toArray(), $final_branches_sp1);
 
-        $final_branches = array_intersect($final_branches_sp2, explode(',', $auth_user->branch_id));
-        $final_branches = empty($final_branches) ? [null] : $final_branches;
+            $final_branches = array_intersect($final_branches_sp2, explode(',', $auth_user->branch_id));
+            $final_branches = empty($final_branches) ? [null] : $final_branches;
+        }
 
         $cashiers_branches = Branch::whereDeleted(false)
                                 ->where(function ($query) use ($final_branches) {

4a9a3bd8d80b745428e657ba1d4349a7f18e36a4
unoitdev <unoitdev@gmail.com>
2024-02-19 09:28:28 +0800

    Route API - add maintained distributor checker

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\api\DistributorController;
 use App\Http\Controllers\api\ErpNextController;
 use App\Http\Controllers\api\ItemController;
 use App\Http\Controllers\api\ShippingFeeController;
+use App\Http\Controllers\api\TransactionTypeController;
 use App\Http\Controllers\api\PermissionController;
 
 /*
@@ -25,11 +26,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('distributor/{id}', [DistributorController::class, 'get_distributor_by_id']);
+Route::get('distributor/maintained/{id}', [DistributorController::class, 'get_maintained_distributor_by_id']);
+
 Route::get('item/transaction_type/{transaction_type_id}', [ItemController::class, 'get_item_by_transaction_type']);
 Route::get('item/stock_by_warehouse/{item_id}/{branch_id}', [ItemController::class, 'get_stock_by_warehouse']);
 Route::get('item/{id}', [ItemController::class, 'get_item_by_id']);
 Route::get('item/bundle/{bundle_code}', [ItemController::class, 'get_item_bundle']);
 Route::get('shippingfee/{id}', [ShippingFeeController::class, 'get_shippingfee_by_id']);
+Route::get('transaction_type/is_upc_ubc/{id}', [TransactionTypeController::class, 'is_upc_ubc_transaction']);
 Route::get('branches_by_cashiers_id/{cashiers_id}/{auth_user_id}', [BranchController::class, 'get_branches_by_cashiers_id']);
 
 Route::post('permission', [PermissionController::class, 'user_permission']);
